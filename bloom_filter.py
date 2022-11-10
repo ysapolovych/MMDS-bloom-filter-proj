893c3476c670fdd8eb98ac66bb840366c751249c
Yevhen Sapolovych <e.sapolovych@gmail.com>
2022-11-10 21:36:28 +0200

    Add funcs to save/load filter array

--- bloom_filter.py
+++ bloom_filter.py
@@ -2,15 +2,18 @@ import mmh3
 import numpy as np
 
 class BloomFilter:
-    def __init__(self, n_elements, fp_prob=0.1):
+    def __init__(self, filter_path=None, n_elements=1000, fp_prob=0.1):
         # https://ebrary.net/25915/computer_science/hashing_functions
         # bitarray size
         self.size = -int((n_elements * np.log(fp_prob)) / np.log(2)**2)
         # n of hash functions
         self.k = int((self.size * np.log(2)) / n_elements)
         # bitarray
-        self.filter = np.zeros(self.size).astype(int)
-        print(f'Initiazied filter of size {self.size}')
+        if filter_path is None:
+            self.filter = np.zeros(self.size).astype(int)
+            print(f'Initiazied filter of size {self.size}')
+        else:
+            self.filter = self.load_filter(filter_path)
 
     def insert(self, element):
         for i in range(self.k):
@@ -23,3 +26,9 @@ class BloomFilter:
             if self.filter[hash] != 1:
                 return False
         return True
+
+    def save_filter(self, out_path):
+        np.save(out_path, self.filter)
+
+    def load_filter(self, in_path):
+        self.filter = np.load(in_path)
